1748ca2ea41114b6da68e6b9e4d92d3a2b8b8928
EUNHEE JU <ehju222@gmail.com>
2025-07-17 23:31:33 +0900

    feature: before AOP - process timeMs print logic

--- src/main/java/hello/hello_spring/service/MemberService.java
+++ src/main/java/hello/hello_spring/service/MemberService.java
@@ -35,25 +35,18 @@ public class MemberService {
      * 회원가입
      */
     public Long join(Member member) {
-//        ctrl + T : 리팩토링 단축키
 
-          validateDuplicateMember(member);
-//        아래를 간단하게 변환한 내용임.
-//        // 같은 이름이 있는 중복 회원 X
-//        // Command + Option + V : 리턴 해줌.
-//        Optional<Member> result = memberRepository.findByName(member.getName());
-//
-//        // 아까 Optional을 한 번 감싸서 반환해줬고 그 감싼 덕분에 이렇게 '값이 있으면' 사용 가능
-//        // - 아래 두 개 메소드도 많이 사용함.
-//        //      * result.get()
-//        //      * result.orElseGet()
-//        result.ifPresent(m -> {
-//           throw new IllegalStateException("이미 존재하는 회원입니다.");
-//        });
+        long start = System.currentTimeMillis();
 
-       memberRepository.save(member);
-
-       return member.getId();
+        try {
+            validateDuplicateMember(member); //중복 회원 검증
+            memberRepository.save(member);
+            return member.getId();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("join = " + timeMs + "ms");
+        }
     }
 
     // 중복회원 검증
@@ -68,7 +61,22 @@ public class MemberService {
      * 전체 회원 조회
      */
     public List<Member> findMembers() {
-        return memberRepository.findAll();
+
+//      회원가입, 회원 조회에 시간을 측정하는 기능은 "핵심 관심 사항(Core concern)"이 아니다.
+//      시간을 측정하는 로직은 "공통 관심 사항(Cross-cutting concern"이다.
+//      시간을 측정하는 로직과 핵심 비즈니스의 로직이 섞여서 유지보수가 어렵다.
+//      시간을 측정하는 로직을 별도의 공통 로직으로 만들기 매우 어렵다.
+//      시간을 측정하는 로직을 변경할 때 모든 로직을 찾아가면서 변경해야 한다.
+
+        long start = System.currentTimeMillis();
+
+        try {
+            return memberRepository.findAll();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("findMembers = " + timeMs + "ms");
+        }
     }
 
     public Optional<Member> findOne(Long id) {
